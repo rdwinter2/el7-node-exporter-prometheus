52deb87fbbac437ccbf24fc9494d84b2f78a2b27
Robert D. Winter, 2nd <rdwinter2@gmail.com>
2019-04-17 09:23:50 -0500

    variable corrected

--- tests/test.sh
+++ tests/test.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
-cd /opt/ansible && \
-. /opt/ansible/hacking/env-setup -q
+# sudo yum install -y gcc openssl-devel bzip2-devel
+# cd /usr/src
+# sudo wget https://www.python.org/ftp/python/2.7.16/Python-2.7.16.tgz
+# sudo tar xzf Python-2.7.16.tgz
+# cd Python-2.7.16
+# sudo ./configure --enable-optimizations
+# sudo make altinstall
+# /usr/local/bin/python2.7 -V
+# sudo curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+# sudo python2.7 get-pip.py
+# sudo git clone https://github.com/ansible/ansible.git --recursive /opt/ansible
+# sudo yum install python-devel python-pip
+# sudo pip install --upgrade pip
+# sudo git clone git://github.com/pallets/jinja.git /usr/lib/python2.7/site-packages/jinja2
+# sudo pip install jinja2
+#alias python="python2.7"
+#cd /opt/ansible && \
+#/bin/bash /opt/ansible/hacking/env-setup -q
 
-PATH=/opt/ansible/bin:$PATH
+# go back to where we were
+#cd -
+
+#PATH=/opt/ansible/bin:$PATH
+# sudo pip install ansible
 
 ansible-playbook -v test.yml -i inventory --forks 5 --syntax-check
 
